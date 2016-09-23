e7bcbbf7aab7288407cf780073e0949a48fc1225
Alexander Peter <Alexander Peter>
2016-09-23 08:31:44 +0300

    Init commit.

---
+++ UpdateManager/Core/Device.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace UpdateManager.Core
+{
+    class Device
+    {        
+        public string deviceId { get; set; }
+        public List<string> hardwareId { get; set; }
+        public string status { get; set; }
+        public string statusCode { get; set; }
+    }
+
+    class OSinfo
+    {
+        public string arch { get; set; }
+        public string ver { get; set; }
+    }
+
+    class InfoDrivers
+    {
+        public List<Device> devices { get; set; }
+        public OSinfo windows { get; set; }
+    }
+}
---
+++ UpdateManager/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Windows.Forms;
+using UpdateManager.Core;
+
+namespace UpdateManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InfoDrivers data = new InfoDrivers();
+            data.devices = getDrivers();
+            data.windows = getInfoAboutOS();
+            string ad = JsonConvert.SerializeObject(data);            
+        }
+        List<Device> getDrivers()
+        {
+            List<Device> arr = new List<Device>();
+
+            ManagementObjectSearcher drivers = new ManagementObjectSearcher("Select * FROM Win32_PnPEntity");
+
+            foreach (ManagementObject obj in drivers.Get())
+            {                
+                Device temp = new Device();
+
+                temp.deviceId = object.Equals(obj.GetPropertyValue("DeviceID"), null) ? string.Empty : obj.GetPropertyValue("DeviceID").ToString();                
+                var a = (string[])obj.GetPropertyValue("HardWareID");                
+                temp.hardwareId = object.Equals(a, null) ? null : a.ToList();                
+                temp.status = object.Equals(obj.GetPropertyValue("Status"), null) ? string.Empty : obj.GetPropertyValue("Status").ToString();
+                temp.statusCode = (temp.status == "Error") ? "28" : "0";
+                
+                arr.Add(temp);                
+            }            
+            return arr;
+        }
+        OSinfo getInfoAboutOS()
+        {
+            ManagementObjectSearcher infoOC= new ManagementObjectSearcher("Select * FROM Win32_OperatingSystem");
+            OSinfo temp = new OSinfo();
+            foreach (ManagementObject obj in infoOC.Get())
+            {
+                temp.ver = object.Equals(obj.GetPropertyValue("Version"),null) ? null : obj.GetPropertyValue("Version").ToString().Remove(3);
+                temp.arch = object.Equals(obj.GetPropertyValue("OSArchitecture"), null) ? null : obj.GetPropertyValue("OSArchitecture").ToString().Remove(2);
+            }
+            return temp;
+        }
+    }
+}
