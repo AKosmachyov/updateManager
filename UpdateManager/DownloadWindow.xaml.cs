ed7f63270c5dcb8544345d5766624e1e82f02a19
Alexander Peter <Alexander Peter>
2016-11-11 14:40:40 +0300

    Changed logic for download drivers

--- UpdateManager/DownloadWindow.xaml.cs
+++ UpdateManager/DownloadWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using UpdateManager.Core;
 using UpdateManager.Entity;
 
 namespace UpdateManager
@@ -20,13 +21,18 @@ namespace UpdateManager
     /// </summary>
     public partial class DownloadWindow : Window
     {
+        List<ProgressBar> progressBarsList = new List<ProgressBar>();
+        List<DataGridEntity> driverList = new List<DataGridEntity>();
+
         public DownloadWindow()
         {
             InitializeComponent();
         }
+
         public DownloadWindow(List<DataGridEntity> list)
         {
             InitializeComponent();
+            driverList = list;
             gridWithElements.ColumnDefinitions.Add(new ColumnDefinition());
             gridWithElements.ColumnDefinitions.Add(new ColumnDefinition());
             for (var i = 0; i < list.Count; i++)
@@ -42,17 +48,18 @@ namespace UpdateManager
                 progress.Width = 150;
                 Grid.SetColumn(label, 0);
                 Grid.SetColumn(progress, 1);
+                progressBarsList.Add(progress);
 
                 Grid.SetRow(label, i);
                 Grid.SetRow(progress, i);
                 gridWithElements.Children.Add(label);
                 gridWithElements.Children.Add(progress);
-
             }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
-        {            
+        {
+            WorkWithDriver.downloadDrivers(driverList, progressBarsList);
         }
     }
 }
